2a2e0f9ec1a602d6ef6841c6758c6e9e6b57b295
Pablo Pazmino <elpablopazmino@gmail.com>
2013-01-25 22:48:36 -0500

    optional fiel deppending on the person type

--- app/Model/Cliente.php
+++ app/Model/Cliente.php
@@ -1,52 +1,77 @@
 <?php
 class Cliente extends AppModel {
-	
+
 	public $name = 'Cliente';
-  public $useTable = 'clientes';
+	public $useTable = 'clientes';
+
 	public $belongsTo = array(
-		'Persona' => array(
-			'className'    => 'Persona',
-			'foreignKey'   => 'persona_id'
+			'Persona' => array(
+					'className'    => 'Persona',
+					'foreignKey'   => 'persona_id'
 			),
-		'Cargo' => array(
-			'className'    => 'Cargo',
-			'foreignKey'   => 'cargo_id'
+			'Cargo' => array(
+					'className'    => 'Cargo',
+					'foreignKey'   => 'cargo_id'
 			),
-		'Division' => array(
-			'className'    => 'Division',
-			'foreignKey'   => 'division_id'
+			'Division' => array(
+					'className'    => 'Division',
+					'foreignKey'   => 'division_id'
 			),
-		'Categoria' => array(
-			'className'    => 'Categoria',
-			'foreignKey'   => 'categoria_id'
+			'Categoria' => array(
+					'className'    => 'Categoria',
+					'foreignKey'   => 'categoria_id'
 			),
 			'Zona' => array(
-				'className'    => 'Zona',
-				'foreignKey'   => 'zona_id'
-				),
+					'className'    => 'Zona',
+					'foreignKey'   => 'zona_id'
+			),
 			'Especializacion' => array(
-				'className'    => 'Especializacion',
-				'foreignKey'   => 'especializacion_id'
-				),
-			);
-	public $validate = array(
-        'cargo_id' => array(
-            'rule' => 'notEmpty'
-        ),
-        'contacto_pedidos' => array(
-            'rule' => 'notEmpty'
-        ),
-        'contacto_cobros' => array(
-            'rule' => 'notEmpty'
-        ),
-        'direccion_calle_principal' => array(
-          'rule' => 'notEmpty'  
-        ),
-        'zona_id' => array(
-          'rule' => 'notEmpty'  
-        ),
-    );
+					'className'    => 'Especializacion',
+					'foreignKey'   => 'especializacion_id'
+			),
+	);
+	public $validate = array('cargo_id' => array(
+			'rule' => 'notEmpty'
+	),
+			'contacto_pedidos' => array(
+					array('rule' => array('optionalOnNaturalPedidos', 'contacto_pedidos'),'notEmpty'
+					),
+					'contacto_cobros' =>
+					array('rule' => array('optionalOnNaturalCobros', 'contacto_cobros'),'notEmpty'
+					),
+					'direccion_calle_principal' => array(
+							'rule' => 'notEmpty'
+					),
+					'zona_id' => array(
+							'rule' => 'notEmpty'
+					)
+			));
 
+	function optionalOnNaturalPedidos(){
+		$passed = true;
+		if($this->data['Persona']['tipo_de_persona'] == '0')
+			return $passed;
+
+
+		if( strlen($this->data['Persona']['contacto_pedidos']) >0 ) {
+			$passed = true;
+		}else{
+			$passed = false;
+		}
+		return $passed;
+	}
+	function optionalOnNaturalCobros(){
+		$passed = true;
+		if($this->data['Persona']['tipo_de_persona'] == '0')
+			return $passed;
 	
+	
+		if( strlen($this->data['Persona']['contacto_cobros']) >0 ) {
+			$passed = true;
+		}else{
+			$passed = false;
+		}
+		return $passed;
+	}
 	
 }
